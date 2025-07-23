590995dcac2383dfbd5d65b3fc8866a42b8ba1bb
Sunday Oruwhone <sundayoruwhone@gmail.com>
2025-07-23 13:09:58 +0100

    Added types.

---
+++ src/types/types.tsx
@@ -0,0 +1,26 @@
+export interface UserTableItem {
+  key: string | number;
+  name:
+    | "Super admin"
+    | "Merchant admin"
+    | "Support admin"
+    | "Sales personnel"
+    | "Deputy sales personnel"
+    | "Developer admin"
+    | "Developer basic";
+  type: "DEFAULT" | "CUSTOM" | "SYSTEM-CUSTOM";
+  dateCreated: string;
+  status: "Active" | "Inactive";
+  users: string[];
+  userCount: number;
+}
+
+export interface AvailableRolesItem {
+  value: "super admin" | "developer admin" | "support admin";
+  label: "Super admin" | "Developer admin" | "Support admin";
+  lastActive: string;
+  isActive: boolean;
+}
+
+export type UsersTableDataType = UserTableItem[];
+export type AvailableRolesType = AvailableRolesItem[];
