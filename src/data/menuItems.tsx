a27d43e010ca280119bdcadbd4c961ab16d1d042
Sunday Oruwhone <sundayoruwhone@gmail.com>
2025-07-23 19:42:51 +0100

    Added label count to 'Dashboard' menu label.

--- src/data/menuItems.tsx
+++ src/data/menuItems.tsx
@@ -19,7 +19,14 @@ export const menuItems: MenuProps["items"] = [
   {
     key: "dashboard",
     icon: <ChartNoAxesColumn size={16} />,
-    label: "Dashboard",
+    label: (
+      <div className="flex justify-between items-center w-full">
+        <span>Dashboard</span>
+        <span className="bg-[var(--grey-200)] text-[var(--grey-900)] text-xs px-2 py-0.5 rounded-full">
+          10
+        </span>
+      </div>
+    ),
   },
   {
     key: "projects",
